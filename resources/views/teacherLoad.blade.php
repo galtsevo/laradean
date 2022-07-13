da3a683b63f87a98fa8fc4e2c02a6a90924685b9
KamishiroIyamoto <55036694+KamishiroIyamoto@users.noreply.github.com>
2022-07-13 11:34:11 +0300

    Add view

---
+++ resources/views/teacherLoad.blade.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/teacherLoad.css">
+</head>
+<body>
+    <div class="container">
+        <div class="selections">
+            <p>Выберите учебный год:</p>
+            <select id="yid">
+                <option selected>Выберите...</option>
+                <option value="22">21/22</option>
+                <option value="23">22/23</option>
+                <option value="24">23/24</option>
+                <option value="25">24/25</option>
+                <option value="26">25/26</option>
+            </select>
+        </div>
+    </div>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.js"></script>
+    <script type="text/javascript" src="../js/teacherLoad.js"></script>
+</body>
+</html>
