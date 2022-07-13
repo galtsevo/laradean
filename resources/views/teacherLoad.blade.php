7691bfa0b3b02a3b60848dda48f19ccd42cbb98b
KamishiroIyamoto <55036694+KamishiroIyamoto@users.noreply.github.com>
2022-07-13 11:49:40 +0300

    Update teacherLoad

--- resources/views/teacherLoad.blade.php
+++ resources/views/teacherLoad.blade.php
@@ -1,14 +1,8 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link rel="stylesheet" href="../css/teacherLoad.css">
-</head>
-<body>
+@extends('layouts.app')
+
+<link href="{{ mix('css/teacherLoad.css') }}" rel="stylesheet">
+
+@section('content')
     <div class="container">
         <div class="selections">
             <p>Выберите учебный год:</p>
@@ -22,8 +16,7 @@
             </select>
         </div>
     </div>
+@endsection
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.js"></script>
-    <script type="text/javascript" src="../js/teacherLoad.js"></script>
-</body>
-</html>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.js"></script>
+<script src="{{ mix('js/search2.js') }}" defer></script>
